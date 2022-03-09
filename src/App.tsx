1f594699d15721f6f9a9a1c22cac8c46dbd47cef
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-09 07:46:39 -0300

    Implementando o styled-component global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>
         hello World!
       </h1>
+      <GlobalStyle/>
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+    -red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --background: #f0f2f5;
+    --shape: #ffffff;
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+/*Ajustando o tamanho da fonte relativo ao dispositivo*/
+/* Font-size padão: 16px (descktop)*/
+html{ 
+    @media (max-width: 1080px) {
+        font-size: 93.75%;
+        /*diminui o font-size para 15px */
+    }
+    @media (max-width: 768px) {
+        font-size: 87.5%;
+        /*diminui o font-size para 14px */
+    }
+    @media (max-width: 425px) {
+        font-size: 81.3%;
+        /*diminui o font-size para 13px */
+    }
+}
+
+body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+}
+
+button{ 
+    cursor: pointer;
+}
+
+[disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+}
+`
