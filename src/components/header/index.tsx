b6be8212aceb342f375e5352c65fe3d64254f761
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-15 21:23:19 -0300

    implementação do React-Modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content} from './styles'
 
 export function Header(){
+    const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setisNewTransactionModalOpen(true)
+    }
+    function handlecloseNewTransactionModal(){
+        setisNewTransactionModalOpen(false)
+        
+    }
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova Transação
                 </button>
+                <Modal 
+                    isOpen={isNewTransactionModalOpen}
+                    onRequestClose={handlecloseNewTransactionModal}
+                >
+
+                </Modal>
             </Content>
         </Container>
     )
