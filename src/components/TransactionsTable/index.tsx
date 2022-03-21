4500604e80346b42d9ae46738a9d30be34e4aa3e
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-20 21:39:20 -0300

    .

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
-interface Transaction{
+interface Transaction {
     id: number;
     title: string;
-    amount:number;
+    amount: number;
     type: string;
     category: string;
-    createdAT:string;
-
+    createdAT: string;
 }
 
 export function TransactionsTable() {
@@ -37,9 +36,18 @@ export function TransactionsTable() {
                     {transactions.map((transaction) => (
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
-                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                }).format(transaction.amount)}
+                            </td>
                             <td>{transaction.category}</td>
-                            <td>{transaction.createdAT}</td>
+                            <td>
+                                {new Intl.DateTimeFormat("pt-BR").format(
+                                    new Date(transaction.createdAT)
+                                )}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
